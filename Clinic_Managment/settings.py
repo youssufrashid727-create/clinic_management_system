88c7e254bf992ccd0cb9b2c1a4450e48aba03d59
youssufrashid727-create <youssufrashid727@gmail.com>
2026-09-10 13:43:20 +0300

    Add Procfile, gunicorn, and production settings

--- Clinic_Managment/settings.py
+++ Clinic_Managment/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -104,7 +105,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MAILERS = {
