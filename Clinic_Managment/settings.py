7bcd491885815a49b43a067a23dc22e700afca84
youssufrashid727-create <youssufrashid727@gmail.com>
2026-09-10 11:00:31 +0300

    Add Procfile, gunicorn, and production settings

--- Clinic_Managment/settings.py
+++ Clinic_Managment/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Clinic_Managment.wsgi --log-file -
